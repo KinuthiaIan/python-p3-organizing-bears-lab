2dc0b9d7aed03473ab6fe4bb0078e29970b977c5
Ian Kinuthia 254 <144425644+KinuthiaIan@users.noreply.github.com>
2024-03-20 21:50:37 +0300

    Add files via upload

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 10, "M", "brown", "high", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 5, "M", "black", "moderate", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 6, "F", "brown", "low", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 15, "M", "brown", "very high", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 8, "F", "pink", "low", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 18, "M", "black", "high", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 4, "F", "purple", "very low", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 10, "M", "brown", "high", 1);
